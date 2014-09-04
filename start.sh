aa5d9525f7f46f85971950cea62a1e5c0e6ca501
Emil Madsen <sovende@gmail.com>
2014-09-05 01:09:25 +0200

    Updated the start scripts.

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-./ircbot/ircbot &
-tail -3 -f a.log | sed "s/å/aa/g" | sed "s/æ/ae/g" | sed "s/ø/oe/g" | ./irc /dev/ttyACM0
+tail -f a.log | sed -u "s/å/aa/g" | sed -u "s/æ/ae/g" | sed -u "s/ø/oe/g" | ./irc /dev/ttyACM0
